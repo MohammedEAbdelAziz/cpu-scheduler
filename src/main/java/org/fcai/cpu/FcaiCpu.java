db9bcc478c804233f60b7e99b00733b16a46fa91
MohammedEAbdelAziz <Mohammedessamabdelaziz@outlook.com>
2024-12-06 19:36:55 +0200

    initial commit

---
+++ src/main/java/org/fcai/cpu/FcaiCpu.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.fcai.cpu;
+
+/**
+ *
+ * @author moham
+ */
+public class FcaiCpu {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
